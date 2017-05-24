88a77d1505f3644c36b5317b0e68317418c3dcf4
Ana Trisovic <ana.trisovic@gmail.com>
2017-05-24 17:09:36 +0200

    Production request parced for DB

--- getProductions.py
+++ getProductions.py
@@ -1,10 +1,19 @@
 import json
+from datetime import datetime
 import cPickle
 from DIRAC.Core.Base import Script
 Script.parseCommandLine( ignoreErrors = True )
 from DIRAC.Core.DISET.RPCClient      import RPCClient
 from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient import BookkeepingClient
+import re
 bk  = BookkeepingClient()
+
+def json_serial(obj):
+   """JSON serializer for objects not serializable by default json code"""
+   if isinstance(obj, datetime):
+           serial = obj.isoformat()
+           return serial
+   raise TypeError ("Type not serializable")
 
 serviceFields = [ 'RequestID', 'HasSubrequest', 'ParentID', 'MasterID',
                     'RequestName', 'RequestType', 'RequestState',
@@ -50,38 +59,65 @@ pr = RPCClient( "ProductionManagement/ProductionRequest" )
 
 filter = {"RequestState":"Done","RequestType":'Simulation'}
 #res = pr.getProductionRequestList( 0, '', 'ASC', 0, 0, {"RequestType":'Simulation','IsModel':1} )['Value']
-res = pr.getProductionRequest([38608])
-#res = pr.getProductionRequestList( 0, '', 'ASC', 0, 0, {'RequestID':[38608]} )#['Value']
-#rows = [converta( x ) for x in res['Rows']]
+#res = pr.getProductionRequest([38608])
+res = pr.getProductionRequestList( 0, '', 'ASC', 0, 0, {'RequestID':[38608, 38610]} )['Value']
 
 ############################################
-print res
-
+#print res
+#print res.keys
+text = res['Rows'][0]["ProDetail"]
+#print json.dumps(text, default=json_serial, indent=4, separators=(',', ': '))
+# Count steps
+stepno = 0
+temp = re.findall(r"p\dStep", text)
+stepno = len(temp)
+#print temp, stepno
 # Create production dict
-'''
+
 prod = dict()
-for i in range(1,10):
+# Take all the useful stuff
+prod["SimCondition"] = res['Rows'][0]["SimCondition"]
+prod["RequestType"] = res['Rows'][0]["RequestType"]
+prod["EventType"]= res['Rows'][0]["EventType"]
+prod["NumberOfEvents"]= res['Rows'][0]["NumberOfEvents"]
+prod["ProPath"]= res['Rows'][0]["ProPath"]
+prod["RequestID"]= res['Rows'][0]["RequestID"]
+prod["upTime"]= res['Rows'][0]["upTime"]
+prod["crTime"]= res['Rows'][0]["crTime"]
+# Ditch the rest
+rows = [converta( x ) for x in res['Rows']]
+# print json.dumps(rows, default=json_serial, indent=4, separators=(',', ': '))
+
+
+current = {}
+for i in range(1, stepno):
   stepid = 'p%dStep' % i
-  if stepid in rows[1]:
+  if stepid in rows[0]:
     if rows[0][stepid]:
-      res = bk.getAvailableSteps({'StepId':rows[0][stepid]})
-      if not res['OK']:
-        print res['Message']
+      a = bk.getAvailableSteps({'StepId':rows[0][stepid]})
+      if not a['OK']:
+        print a['Message']
         exit(1)
-      prod[stepid] = res['Value']
+      values = a["Value"]["Records"][0]
+      keys = values[-1]["ParameterNames"]
+      step = {}
+      for i in range(len(keys)):
+              step[keys[i]] = values[i]
+      prod[stepid] = step
+
 passes = []
-for i in range(1,10):
+for i in range(1, stepno):
    stepid = 'p%dUse' % i
    procid = 'p%dPass' % i
    if stepid in rows[0]:
      if rows[0][stepid] == 'Yes':
         passes.append(rows[0][procid])
 
-prod[bkkpath] = "/".join(passes)
-json.dumps(prod)
+prod["bkkpath"] = "/".join(passes)
 
+print json.dumps(prod, default=json_serial, indent=4, separators=(',', ': '))
 #res = pr.getProductionRequestList( 0, '', 'ASC', 0, 0, {'RequestID':[38608,38610,38609,38603]} )['Value']
-'''
+
 #print rows[0]
 
 
